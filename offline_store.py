cd952cea1fc2e3d9f7c969191028149eff8b41f9
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2023-11-27 13:24:24 -0800

    Pushed from Domino: https://ghtraining.cs.domino.tech/workspace/integration-test/FeatureStore?executionId=6564ce631fe1f813f9c466f7

--- offline_store.py
+++ offline_store.py
@@ -22,15 +22,11 @@ current = Path.cwd()
 # Read form snowflake
 
 driver_stats_source = SnowflakeSource(
-    
     database='FEAST', 
-   
     table='DRIVER_STATS',
-    
-# The event timestamp is used for point-in-time joins and ensuring that only features within the TTL are returned.
-    
+    # The event timestamp is used for point-in-time joins and ensuring that only features within the TTL are returned.
     event_timestamp_column="event_timestamp",
-# The (optional) created timestamp is used to ensure there are no duplicate feature rows in the offline store or when building training datasets.
+    # The (optional) created timestamp is used to ensure there are no duplicate feature rows in the offline store or when building training datasets.
     created_timestamp_column="created",
 ) 
 
